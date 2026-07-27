209ee6934dde43bf63e9982bc42f9a3e217868b6
Subhash Tarfewad <subhashtarfewad@gmail.com>
2026-07-28 00:30:21 +0530

    Time: 8 ms (82.33%), Space: 11.8 MB (54.52%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -10,14 +10,26 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         /*using Map Function*/
-        map<ListNode*,int> mpp;
-        ListNode* temp=head;
-        while(temp!=NULL){
-            if(mpp.find(temp)!=mpp.end()){
+        // map<ListNode*,int> mpp;
+        // ListNode* temp=head;
+        // while(temp!=NULL){
+        //     if(mpp.find(temp)!=mpp.end()){
+        //         return true;
+        //     }
+        //     mpp[temp]=1;
+        //     temp=temp->next;
+        // }
+        // return false;
+
+        /*Tortoise & Hare Algorithm*/
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+             if(fast==slow){
                 return true;
             }
-            mpp[temp]=1;
-            temp=temp->next;
         }
         return false;
     }
